01485d8e8b6c370c9a041142c58426906220368f
Alexandr Tkach <can4o1992@gmail.com>
2021-02-03 23:11:06 +0300

    feat: add routes and controllers for shops

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userRouter = require('./routes/user.routes');
 const cityRouter = require('./routes/cities.routes');
+const shopRouter = require('./routes/shops.routes');
 
 const PORT = process.env.PORT || 8080;
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use('/', userRouter);
 app.use('/', cityRouter);
+app.use('/', shopRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
